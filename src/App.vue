2c34b7459cb71c9c59125e697ea0027c6374bf46
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-11-11 18:59:34 -0300

    Continue...

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <HeaderView :totalIncome="state.totalIncome" />
-    <FormView @add-income="AddIncome" />
+    <FormView  :state="state" @add-income="AddIncome" />
+    <IncomeList :state="state" @remove-item="removeItem" />
   </div>
 </template>
 
@@ -9,41 +10,58 @@
 import { reactive, computed } from "vue";
 import HeaderView from "./components/HeaderView.vue";
 import FormView from "./components/FormView.vue";
+import IncomeList from "./components/IncomeList.vue"
 
 export default {
   setup() {
     const state = reactive({
       income: [],
+      sortedIncome: computed(() => {
+        let temp = [];
+        
+        temp = state.income.sort((a, b) => {
+          return b.date - a.date;
+        });
+        return temp;
+      }),
+
       totalIncome: computed(() => {
         let temp = 0;
-
         if (state.income.length > 0) {
           for (let i = 0; i < state.income.length; i++) {
             temp += state.income[i].value;
           }
-        }
         return temp;
+        } else {
+          return 0;
+        }
       }),
     });
 
     function AddIncome(data) {
       let d = data.date.split("-")
-      let newD = new Date(d[0], d[1], d[2])
+      console.log("d >>>", d);
+      console.log(new Date());
+      let newD = new Date(d[0], d[1] -1, d[2])
+      console.log("newD >>>", newD);
 
       state.income = [...state.income, {
       id: Date.now(),
       desc: data.desc,
-      value: data.value,
+      value: parseInt(data.value),
       date: newD.getTime()
       }]
+      // console.log(state.income);
     }
     return {
       state,
+      AddIncome
     };
   },
   components: {
     HeaderView,
     FormView,
+    IncomeList
   },
 };
 </script>
---
+++ src/components/IncomeList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="income-list">
+    <IncomeItem v-for="data in state.sortedIncome" :key="data.id" :income="data" />
+  </div>
+</template>
+
+<script>
+import IncomeItem from "./IncomeItem.vue";
+export default {
+  components: {
+    IncomeItem,
+  },
+  props: {
+    state: Object,
+  },
+};
+</script>
+
+<style scoped>
+.income-list {
+  margin-top: 30px;
+  padding: 15px;
+}
+</style>
